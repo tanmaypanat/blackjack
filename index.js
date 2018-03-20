178c6d3509f6cda55bc1a0beada7d5b0d8859f66
tanmaypanat <panattanmay@gmail.com>
2018-03-20 10:56:04 -0400

    old again

--- index.js
+++ index.js
@@ -43,15 +43,17 @@ app.get('/hit/:ninerId', (req, res) => {
         d_first = d_card[0]
         dealerTotal = d_card[0] + d_card[1];
         userObj[req.params.ninerId] = t_card[0] + t_card[1];
-        let sender=[];
-        sender[0]=t_card;
-        sender[1]=  userObj[req.params.ninerId];
-          sender[2]=d_first;
+       // let sender=[];
+        //sender[0]=t_card;
+        //sender[1]=  userObj[req.params.ninerId];
+          //sender[2]=d_first;
           //res.send(sender);
-        var myJsonString = JSON.stringify(sender);
-        res.json([
-          sender
-        ]);
+       // var myJsonString = JSON.stringify();
+        res.json({
+          cards : t_card,
+          total : userObj[req.params.ninerId],
+          dealerHand : [d_first]
+        });
       } else {
         let s_card = random_item(cards);
         userObj[req.params.ninerId] += s_card;
