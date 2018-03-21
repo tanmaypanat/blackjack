a464cf165920e4262188757d756336c2573d7bf9
tanmaypanat <panattanmay@gmail.com>
2018-03-21 14:45:27 -0400

    ni

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/join/:ninerId', (req, res) => {
     num=4-count;
     if(num==0)
         {
-            res.send("PLayer 1 : "+players[0]+"\nPlayer 2 : "+players[1]);
+            res.send("PLayer 1 : "+players[0]+"  Player 2 : "+players[1]+"  Player 3 : "+players[2]+"  Player 4 : "+players[3]);
         }
     res.send("Waiting for "+num+" more player to join");
     
