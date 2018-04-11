dd7735eb2bab37ad6a100281c5c300ee02c575fa
tanmaypanat <panattanmay@gmail.com>
2018-04-11 18:42:59 -0400

    n

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/startGame/:ninerId', (req, res) => {
     //def.ninerid=10;
     //def.sum=0;
     //def.useable=false;
-    var tt=new Users(10);
+    var tt=new Users(req.params.ninerId);
     res.send(""+tt.id);
     
 })
